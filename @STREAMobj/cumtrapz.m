8a9fcb0effafe5664998617fe8a492d16a1992cf
Wolfgang Schwanghart <schwanghart@gmail.com>
2015-11-23 10:44:23 +0100

    Create cumtrapz.m

---
+++ @STREAMobj/cumtrapz.m
@@ -0,0 +1,60 @@
+function z = cumtrapz(S,G)
+
+% Cumulative trapezoidal numerical integration along a stream network
+%
+% Syntax
+%
+%     z = cumtrapz(S,G)
+%     z = cumtrapz(S,g)
+%
+% Description
+%
+%     cumtrapz(S,G) computes the cumulative integral of G with respect to
+%     the distance along the stream network S using trapezoidal
+%     integration. S must be a stream network. The second input must either
+%     be a GRIDobj G or a node attribute list g.
+%     
+% Input arguments
+%
+%     S     STREAMobj
+%     G     GRIDobj
+%     g     node attribute list
+%
+% Output arguments
+%
+%     z     node attribute list
+%
+% Example
+%
+%     DEM = GRIDobj('srtm_bigtujunga30m_utm11.tif');
+%     FD = FLOWobj(DEM,'preprocess','carve');
+%     S = STREAMobj(FD,'minarea',1000);
+%     A = flowacc(FD);
+%     a = getnal(S,A)*DEM.cellsize^2;
+%     ghat = 1./(a.^0.45);
+%     z = cumtrapz(S,ghat);
+%     plotdz(S,z)
+%
+% See also: STREAMobj, STREAMobj/gradient
+%
+% Author: Wolfgang Schwanghart (w.schwanghart[at]geo.uni-potsdam.de)
+% Date: 23. November, 2015
+
+
+% get node attribute list with elevation values
+if isa(G,'GRIDobj')
+    validatealignment(S,G);
+    g = getnal(S,G);
+elseif isnal(S,G);
+    g = G;
+else
+    error('Imcompatible format of second input argument')
+end
+
+z = zeros(size(g));
+d = distance(S,'node_to_node');
+
+for r = numel(S.ix):-1:1;
+    z(S.ix(r)) = z(S.ixc(r)) + g(S.ix(r))*d(S.ix(r));
+end
+
