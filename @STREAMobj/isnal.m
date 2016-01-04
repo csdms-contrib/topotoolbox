30ac353ef5c30f7ff67830a88b54a70c2441eba7
Wolfgang Schwanghart <schwanghart@gmail.com>
2016-01-04 22:40:44 +0100

    Create isnal.m
    test whether a vector is a node attribute list of a STREAMobj

---
+++ @STREAMobj/isnal.m
@@ -0,0 +1,31 @@
+function tf = isnal(S,nal)
+
+% test whether a vector is a node attribute list of a STREAMobj
+%
+% Syntax
+%
+%     tf = isnal(S,nal)
+%
+% Description
+%
+%     Node attribute lists are attribute values for each node in the stream
+%     network STREAMobj. This function tests whether a vector nal is a
+%     valid node attribute list of the network S.
+%
+% Input arguments
+%
+%     S      STREAMobj
+%     nal    node attribute list
+%
+% Output arguments
+%
+%     tf     true or false
+%
+%
+% See also: STREAMobj, STREAMobj/getnal
+% 
+% Author: Wolfgang Schwanghart (w.schwanghart[at]geo.uni-potsdam.de)
+% Date: 22. June, 2015
+
+
+tf = isequal(numel(S.IXgrid),numel(nal));
