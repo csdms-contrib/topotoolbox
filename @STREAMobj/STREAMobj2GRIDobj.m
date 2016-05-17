a1fa0c4f21ac8a81a6ff1bc4a4c649d945d035bb
Wolfgang Schwanghart <schwanghart@gmail.com>
2016-05-17 22:40:55 +0200

    Changes to STREAMobj2GRIDobj
    STREAMobj2GRIDobj takes a node-attribute list as input argument

--- @STREAMobj/STREAMobj2GRIDobj.m
+++ @STREAMobj/STREAMobj2GRIDobj.m
@@ -1,30 +1,48 @@
-function G = STREAMobj2GRIDobj(S)
+function G = STREAMobj2GRIDobj(S,nal)
 
 % convert an instance of STREAMobj to an instance of GRIDobj
 %
 % Syntax 
 %
 %     G = STREAMobj2GRIDobj(S)
+%     G = STREAMobj2GRIDobj(S,nal)
 %
 % Description
 %     
 %     STREAMobj2GRIDobj converts an instance of STREAMobj S to a new instance
 %     of GRIDobj G with the same spatial reference and extent as the instance
-%     from which S has been derived.
+%     from which S has been derived. If the second input is a
+%     node-attribute list, STREAMobj2GRIDobj will write the node values to
+%     the respective locations in the output GRIDobj G.
 %     
 % Input arguments
 %
 %     S     STREAMobj
+%     nal   node attribute list
+%
+% Output arguments
+%
 %     G     GRIDobj (contains logical raster with ones at stream locations
-%           and zeros elsewhere.
+%           and zeros elsewhere. If a node attribute list was supplied, G
+%           will have the same underlying class as nal. All non-stream
+%           values are set to nan.
 %
 %
 % See also: STREAMobj, GRIDobj
 % 
 % Author: Wolfgang Schwanghart (w.schwanghart[at]geo.uni-potsdam.de)
-% Date: 22. March, 2013
+% Date: 17. May, 2016
 
-narginchk(1,1)
+narginchk(1,2)
+
+if nargin == 1;
+    nal = true(size(S.x));
+else
+    if ~isnal(S,nal);
+        error('TopoToolbox:isnal',...
+            'The second input argument is not a valid node attribute list');
+    end
+end
 
 G = GRIDobj([]);
 
@@ -40,8 +58,17 @@ for r = 1:numel(six);
     G.(props_G{gix(r)}) = S.(props_S{six(r)});
 end
 
-G.Z = false(G.size);
-G.Z(S.IXgrid) = true;
+
+if islogical(nal)
+    G.Z = false(G.size);
+elseif isinteger(nal)
+    G.Z = zeros(G.size,'like',nal);
+elseif isfloat(nal); 
+    G.Z = nan(G.size,'like',nal);
+end
+G.Z(S.IXgrid) = nal;
+
+    
 
 G.name = 'stream grid';
 
