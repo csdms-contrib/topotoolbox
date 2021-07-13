ca766a8877f2bb2952ea4b4b839883bcd634d08f
Wolfgang Schwanghart <schwanghart@gmail.com>
2021-07-13 22:18:43 +0200

    additions and modifications to PPS

--- @PPS/intensity.m
+++ @PPS/intensity.m
@@ -1,11 +1,11 @@
-function [in,inci] = intensity(P,alpha)
+function [in,inci,pd] = intensity(P,alpha)
 
 %INTENSITY calculate intensity (density) of points on the stream network
 %
 % Syntax
 %
 %     in = intensity(P)
-%     [in,inci] = intensity(P,alpha);
+%     [in,inci,pd] = intensity(P,alpha);
 %
 % Description
 %
@@ -15,20 +15,48 @@ function [in,inci] = intensity(P,alpha)
 %
 %     P      point pattern on stream network (class PPS)
 %     alpha  significance level (default = 0.05)
+%     pd     instance of prob.GammaDistribution. Contains the probability
+%            distribution of the intensity (see Furbish 2021)
 %
 % Output arguments
 %
 %     in     intensity (scalar)
 %     inci   confidence bounds of intensity (two element vector)
 %
+% Example
+%
+%     DEM = GRIDobj('srtm_bigtujunga30m_utm11.tif');
+%     FD  = FLOWobj(DEM,'preprocess','c');
+%     S = STREAMobj(FD,'minarea',1000);
+%     S = removeshortstreams(S,100);
+%     S = clean(S);
+%     P = PPS(S,'rpois',0.001,'z',DEM);
+%     [int,inci,pd] = intensity(P);
+%
+%     % Plot intensity estimate
+%     ci = icdf(pd,[.0001 .9999]);
+%     inti = linspace(ci(1),ci(2),500);
+%     p  = pdf(pd,inti);
+%     plot(inti,p)
+%     xlabel('\lambda [m^{-1}]')
+%     ylabel('f_{\lambda} [m]')
+%     hold on
+%     xline(inci,'k--')
+%     xline(int,'k')
+%     legend('PDF','lower CI 95%','upper CI 95%','mean') 
+%
 % See also: PPS, PPS/npoints 
 %
+% Reference: Furbish (2021): The “uncertainty principle” of a Poisson point 
+%            process.
+% https://my.vanderbilt.edu/davidjonfurbish/files/2013/06/Uncertainty-Principle.pdf
+%
 % Author: Wolfgang Schwanghart (w.schwanghart[at]geo.uni-potsdam.de)
-% Date: 11. February, 2019
+% Date: 13. July, 2021
 
 n  = npoints(P);
-l  = tlength(P);
-in = n/l;   
+s  = tlength(P);
+in = n/s;   
 if nargout > 1
     % If error bounds required
     if nargin == 1
@@ -37,10 +65,16 @@ if nargout > 1
         validateattributes(alpha,{'numeric'},{'>',0,'<',1},...
             'intensity','alpha',2);
     end
-    % Fit loglinear model with intercept only
-    mm = fitloglinear(P,getnal(P.S)+1,'intercept',false);
-    [p,pci]   = predict(mm,1,'alpha',alpha);
-    d   = l./numel(P.S.x);
-    in = p./d; %.cellsize;   
-    inci = pci./d;
+    
+    pd = makedist('Gamma','a',n,'b',1/s);
+    % int = mean(pd);
+    inci  = icdf(pd,[alpha/2 1-alpha/2]);
+
+
+%     % Fit loglinear model with intercept only
+%     mm = fitloglinear(P,getnal(P.S)+1,'intercept',false);
+%     [p,pci]   = predict(mm,1,'alpha',alpha);
+%     d   = l./numel(P.S.x);
+%     in = p./d; %.cellsize;   
+%     inci = pci./d;
 end
---
+++ @PPS/modify.m
@@ -0,0 +1,53 @@
+function [P,nalix,markix] = modify(P,varargin)
+
+%MODIFY modify instance of PPS to meet user-defined criteria
+%
+% Syntax
+%
+%      P2 = modify(P,'pn',pv)
+%
+% Description
+%
+%      PPS/modify modifies the underlying stream network of the PPS object
+%      P. The function is a wrapper for the STREAMobj/modify function but
+%      differs in that it carries the points stored in P along. For a
+%      detailed description, see the help of STREAMobj/modify.
+%
+%      help STREAMobj/modify
+%
+% Input arguments
+%
+%      P        Instance of PPS
+%      'pn',pv  Parameter value pair (see STREAMobj/modify)
+%
+% Output arguments
+%
+%      P2       new instance of PPS
+%      nalix    linear index into node-attribute list underlying P
+%      markix   linear index into marks associated with the point pattern
+%               in P
+%
+%
+% See also: STREAMobj/modify
+%
+% Author: Wolfgang Schwanghart (w.schwanghart[at]geo.uni-potsdam.de)
+% Date: 11. May, 2021
+
+% Apply STREAMobj/modify
+[S,nalix] = modify(P.S,varargin{:});
+
+% Elevations are stored as node attribute list
+if ~isempty(P.z)
+    z = P.z(nalix);
+else
+    z = [];
+end
+
+% Index into markers
+I = ismember(P.S.IXgrid(P.PP),P.S.IXgrid(nalix));
+markix = find(I);
+
+% Finally, create new PPS instance
+P = PPS(S,'PP',P.S.IXgrid(P.PP(I)));
+% Add elevation
+P.z = z;
